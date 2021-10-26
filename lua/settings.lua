50dc4dd78e1dc4043aee8ecc66175e9b46353a7c
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-10-26 22:42:24 +0200

    Add general settings via lua API

---
+++ lua/settings.lua
@@ -0,0 +1,26 @@
+local opt = vim.opt
+local indent = 2
+
+opt.backup = false
+opt.clipboard = 'unnamedplus'
+opt.cmdheight = 2
+opt.cursorline = true
+opt.expandtab = true
+opt.inccommand = 'nosplit'
+opt.lazyredraw = true
+opt.list = true
+opt.listchars = { tab = '▸ ', trail = '█', nbsp = '%' }
+opt.number = true
+opt.pumblend = 15
+opt.relativenumber = true
+opt.shiftwidth = indent
+opt.shortmess:remove("F"):append("c")
+opt.showmode = false
+opt.signcolumn = 'yes:2'
+opt.softtabstop = indent
+opt.tabstop = indent
+opt.termguicolors = true
+opt.title = true
+opt.undofile = true
+opt.updatetime = 300
+opt.writebackup = false
