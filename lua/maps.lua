a12ff38be1829b57f8f691895069219967d501d6
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-10-27 22:37:57 +0200

    Add keyboard mappings that don't require a plugin

---
+++ lua/maps.lua
@@ -0,0 +1,22 @@
+local map = vim.api.nvim_set_keymap
+local nor = { noremap = true }
+local nrs = { noremap = true, silent = true }
+local nre = { noremap = true, expr = true }
+
+vim.g.mapleader = ','
+
+map('c', '%%'               , 'getcmdtype() == ":" ? expand("%:h")."/" : "%%"'    , nre)
+map('n', '+'                , '<c-a>'                                             , nor)
+map('n', '-'                , '<c-x>'                                             , nor)
+map('n', '<c-l>'            , ':<c-u>nohlsearch<cr><c-l>'                         , nrs)
+map('n', '<c-e>'            , '6<c-e>'                                            , nor)
+map('n', '<c-y>'            , '6<c-y>'                                            , nor)
+map('n', '<leader>.'        , ':b#<cr>'                                           , nor)
+map('n', '<leader>r<space>' , ':<c-u>%s/\\s\\+$/<cr>'                             , nor)
+map('n', '<leader>sl'       , ':<c-u>setlocal list!<cr>'                          , nor)
+map('n', '<leader>sw'       , ':<c-u>set wrap!<cr>'                               , nor)
+map('n', '<leader>tn'       , ':<c-u>tabnew<cr>'                                  , nor)
+map('n', '<leader>td'       , ':<c-u>tabclose<cr>'                                , nor)
+map('n', '<leader>yd'       , ':<c-u>let @+ = expand("%:p:h")<cr>'                , nor)
+map('n', '<leader>yff'      , ':<c-u>let @+ = expand("%:p")<cr>'                  , nor)
+map('n', '<leader>yfn'      , ':<c-u>let @+ = expand("%:p") . ":" . line(".")<cr>', nor)
