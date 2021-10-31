50fb02b41373b95bcb4a3d9bd7236d9d9bdebe2f
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-10-31 12:05:46 +0100

    Add packer bootstrap and color schemes

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 require('settings')
 require('maps')
+require('plugins')
+
+function read_if_exists_and_then(filename, f)
+  local file = io.open(filename, 'r')
+  if file then
+    local content = file:read('*all')
+    file:close()
+    f(content)
+  else
+    vim.notify('File not found: ' .. filename, vim.log.levels.WARN)
+  end
+end
+
+read_if_exists_and_then(os.getenv('HOME') .. '/.vimrc_background', vim.cmd)
---
+++ lua/plugins.lua
@@ -0,0 +1,37 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]]
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'lewis6991/impatient.nvim'
+
+  -- colorschemes
+  use 'NLKNguyen/papercolor-theme'
+  use 'arcticicestudio/nord-vim'
+  use 'bluz71/vim-nightfly-guicolors'
+  use 'cocopon/iceberg.vim'
+  use 'dracula/vim'
+  use 'jacoborus/tender'
+  use 'junegunn/seoul256.vim'
+  use 'mhartington/oceanic-next'
+  use 'noah/vim256-color'
+  use 'rakr/vim-one'
+  use { 'rmehri01/onenord.nvim', branch = 'main' }
+  use { 'kaicataldo/material.vim', branch = 'main' }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
