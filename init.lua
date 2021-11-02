e773b77abf43d744686a55348f766e8ed807b7d4
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-11-02 20:56:45 +0100

    Highlight yanked text

--- init.lua
+++ init.lua
@@ -3,4 +3,5 @@ require('settings')
 require('keymaps')
 require('plugins')
 
+vim.cmd [[ au TextYankPost * silent! lua vim.highlight.on_yank {timeout=250, on_visual=false} ]]
 read_if_exists_and_then(os.getenv('HOME') .. '/.vimrc_background', vim.cmd)
