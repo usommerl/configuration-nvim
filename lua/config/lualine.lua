e5c1afead771a7fb3228fc9fb82a746437145971
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-11-04 19:42:54 +0100

    Enable default section seperators

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -5,8 +5,9 @@ end
 
 lualine.setup {
   options = {
-    section_separators = '',
     component_separators = ''
   },
-  sections = { lualine_x = {'encoding', { 'fileformat', icons_enabled = false }, 'filetype'} }
+  sections = {
+    lualine_x = {'encoding', { 'fileformat', icons_enabled = false }, 'filetype'},
+  }
 }
