c5c1774d2309a8efc5731e63b12649180147f497
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-11-21 18:37:17 +0100

    Remove unused code

--- lua/config/nvim-metals.lua
+++ lua/config/nvim-metals.lua
@@ -11,8 +11,3 @@ cmd([[autocmd!]])
 cmd([[autocmd FileType scala setlocal omnifunc=v:lua.vim.lsp.omnifunc]])
 cmd([[autocmd FileType scala,sbt lua require('metals').initialize_or_attach(metals_config)]])
 cmd([[augroup end]])
-
--- Need for symbol highlights to work correctly
---vim.cmd([[hi! link LspReferenceText CursorColumn]])
---vim.cmd([[hi! link LspReferenceRead CursorColumn]])
---vim.cmd([[hi! link LspReferenceWrite CursorColumn]])
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -6,13 +6,6 @@ end
 
 vim.cmd [[packadd packer.nvim]]
 
--- vim.cmd [[
---   augroup packer_user_config
---     autocmd!
---     autocmd BufWritePost plugins.lua source <afile> | PackerCompile
---   augroup end
--- ]]
-
 return require('packer').startup(function(use)
   use { 'wbthomason/packer.nvim' }
   use { 'lewis6991/impatient.nvim' }
