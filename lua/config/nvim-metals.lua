a87637e0a4e3a47f0ac65b43fcd364cc2b529fe6
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-11-21 22:50:35 +0100

    Modify LSP diagnostics virtual text prefix character

--- lua/config/nvim-metals.lua
+++ lua/config/nvim-metals.lua
@@ -24,3 +24,9 @@ for type, icon in pairs(signs) do
     local hl = "LspDiagnosticsSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = {
+    prefix = '|', -- e.g.: '■' '●', '▎', 'x'
+  }
+})
