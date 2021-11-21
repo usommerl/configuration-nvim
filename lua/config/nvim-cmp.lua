fb6a4742ea96c9c542b21bc34bab2400ae822398
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-11-21 19:09:23 +0100

    Enable buffer and path completion sources

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -8,6 +8,8 @@ local lspkind = require('lspkind')
 cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'path' },
   },
   mapping = {
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
