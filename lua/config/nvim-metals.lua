82c2b7eb980748f045956d91cda57dfec28ede0c
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-11-30 21:50:44 +0100

    Adapt to new highlight groups for diagnostics
    See: https://github.com/neovim/neovim/pull/15585/commits/a5bbb932f9094098bd656d3f6be3c58344576709

--- lua/config/nvim-metals.lua
+++ lua/config/nvim-metals.lua
@@ -21,7 +21,7 @@ cmd([[augroup end]])
 
 local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
 for type, icon in pairs(signs) do
-    local hl = "LspDiagnosticsSign" .. type
+    local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
