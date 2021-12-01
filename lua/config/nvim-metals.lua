d486314ffe77cc717b5e1119f991a44b6d7936ee
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2021-12-01 13:54:40 +0100

    Fix diagnostic signs

--- lua/config/nvim-metals.lua
+++ lua/config/nvim-metals.lua
@@ -19,7 +19,7 @@ cmd([[autocmd!]])
 cmd([[autocmd BufWritePre *.scala lua vim.lsp.buf.formatting_sync()]])
 cmd([[augroup end]])
 
-local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
