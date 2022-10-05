319f54618bcb42b8cf9b590d12e6f66bf33e929c
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2022-10-05 13:47:27 +0200

    Set laststatus = 3

--- lua/settings.lua
+++ lua/settings.lua
@@ -4,6 +4,7 @@ local indent = 2
 opt.backup = false
 opt.clipboard = 'unnamedplus'
 opt.cmdheight = 0
+opt.laststatus = 3
 opt.colorcolumn = '9999' -- See: https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
 opt.completeopt = { 'menu', 'noinsert', 'noselect' }
 opt.cursorline = true
