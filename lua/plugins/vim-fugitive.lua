37cc8cc2cd59b1a4b315da5c06c311bf9ff1888b
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-04-16 19:09:18 +0200

    Close fugitive and git buffers by pressing 'q'

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,6 +1,15 @@
 return {
   {
     'tpope/vim-fugitive',
-    version = '^3'
+    version = '^3',
+    config = function()
+      vim.api.nvim_create_autocmd(
+        'FileType',
+        {
+          pattern = { 'fugitive', 'git' },
+          command = [[nnoremap <buffer><silent>q :close<cr>]]
+        }
+      )
+    end
   }
 }
