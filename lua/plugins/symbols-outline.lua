486b8f5519a4d0fc568e76bbc4cbbf4a540e0cff
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-04-19 11:09:46 +0200

    Show relative numbers in symbols outline window

--- lua/plugins/symbols-outline.lua
+++ lua/plugins/symbols-outline.lua
@@ -3,7 +3,10 @@ return {
     'simrat39/symbols-outline.nvim',
     cmd = 'SymbolsOutline',
     config = function()
-      require("symbols-outline").setup()
+      require("symbols-outline").setup({
+        show_numbers = true,
+        show_relative_numbers = true,
+      })
     end
   }
 }
