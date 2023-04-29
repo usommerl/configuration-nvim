835969114d9ccb57041ca470646ba2cb33b7296d
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-04-29 09:39:31 +0200

    Change noice plugin configuration

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -6,6 +6,22 @@ return {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
     },
-    config = function() require("noice").setup() end
+    config = function()
+      require("noice").setup({
+        presets = {
+          command_palette = true
+        },
+        routes = {
+          {
+            filter = {
+              event = "msg_show",
+              kind = "",
+              find = "written",
+            },
+            opts = { skip = true },
+          },
+        },
+      })
+    end
   }
 }
