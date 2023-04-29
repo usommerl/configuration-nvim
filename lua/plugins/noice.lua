85053da35e49f6bbc2bfdb8ec237913d5c15424c
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-04-29 15:50:10 +0200

    Route 'lines yanked' messages to noice mini view

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -21,10 +21,11 @@ return {
             view = "mini",
             filter = {
               any = {
-                { find = "; before #",  event = "msg_show", kind = "" },
-                { find = "; after #",   event = "msg_show", kind = "" },
-                { find = "more lines",  event = "msg_show", kind = "" },
-                { find = "fewer lines", event = "msg_show", kind = "" },
+                { find = "; before #",   event = "msg_show", kind = "" },
+                { find = "; after #",    event = "msg_show", kind = "" },
+                { find = "more lines",   event = "msg_show", kind = "" },
+                { find = "fewer lines",  event = "msg_show", kind = "" },
+                { find = "lines yanked", event = "msg_show", kind = "" },
               },
             },
           },
