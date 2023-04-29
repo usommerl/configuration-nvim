dd40f99ea9f6162368e029fbd2653964caef4915
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-04-29 10:11:04 +0200

    Update noice plugin configuration

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -9,7 +9,8 @@ return {
     config = function()
       require("noice").setup({
         presets = {
-          command_palette = true
+          command_palette = true,
+          lsp_doc_border = true
         },
         routes = {
           {
@@ -22,6 +23,18 @@ return {
           },
         },
       })
+
+      vim.keymap.set({ "n", "i", "s" }, "<c-f>", function()
+        if not require("noice.lsp").scroll(4) then
+          return "<c-f>"
+        end
+      end, { silent = true, expr = true })
+
+      vim.keymap.set({ "n", "i", "s" }, "<c-b>", function()
+        if not require("noice.lsp").scroll(-4) then
+          return "<c-b>"
+        end
+      end, { silent = true, expr = true })
     end
   }
 }
