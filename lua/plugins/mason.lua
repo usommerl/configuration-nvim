08f58c9727848a588b428164ebf3b48c4025fb3b
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-05-03 21:19:51 +0200

    Add snippet engine for nvim-cmp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -32,19 +32,44 @@ return {
         'dartls'
       }
 
-      for _, lsp in pairs(servers) do
-        lspconfig[lsp].setup {
+
+      local on_attach = function(client, bufnr)
+        local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+        buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+        if client.resolved_capabilities.document_highlight then
+          vim.api.nvim_exec([[
+            augroup lsp_document_highlight
+            autocmd! * <buffer>
+            autocmd CursorHold  <buffer> lua vim.lsp.buf.document_highlight()
+            autocmd CursorHoldI <buffer> lua vim.lsp.buf.document_highlight()
+            autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+            augroup END
+            ]],
+            false)
+        end
+      end
+
+      local function make_config()
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.completion.completionItem.snippetSupport = true
+        return {
+          capabilities = capabilities,
           on_attach = on_attach,
         }
       end
 
-      lspconfig.yamlls.setup {
-        settings = {
-          yaml = {
-            keyOrdering = false,
+      for _, lsp in pairs(servers) do
+        local config = make_config()
+        if lsp == "yamlls" then
+          config.settings = {
+            yaml = {
+              keyOrdering = false,
+            }
           }
-        },
-      }
+        end
+        lspconfig[lsp].setup(config)
+      end
     end
   }
 }
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -5,12 +5,19 @@ return {
       { 'hrsh7th/cmp-nvim-lsp' },
       { 'hrsh7th/cmp-buffer' },
       { 'hrsh7th/cmp-path' },
+      { 'hrsh7th/cmp-vsnip' },
+      { 'hrsh7th/vim-vsnip' },
       { 'onsails/lspkind-nvim' },
     },
     config = function()
       local cmp = require('cmp')
       local lspkind = require('lspkind')
       cmp.setup {
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
         sources = {
           { name = 'nvim_lsp' },
           { name = 'buffer' },
@@ -24,7 +31,7 @@ return {
           ['<cr>'] = cmp.mapping.confirm({ select = true }),
         },
         formatting = {
-          format = lspkind.cmp_format({maxwidth = 60})
+          format = lspkind.cmp_format({ maxwidth = 60 })
         }
       }
     end
