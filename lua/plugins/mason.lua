85a8c1a92bcb41d1bde50a27ea777931f4679d6b
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-05-04 10:02:43 +0200

    Update plugins

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -41,7 +41,7 @@ return {
           client.server_capabilities.documentFormattingProvider = true
         end
 
-        if client.resolved_capabilities.document_highlight then
+        if client.server_capabilities.document_highlight then
           vim.api.nvim_exec([[
             augroup lsp_document_highlight
             autocmd! * <buffer>
