c31aaa90a236e7c355e5ed4b0135b88c9e3ed0df
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2023-05-15 22:48:13 +0200

    Show first indent level

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,14 +2,16 @@ return {
   {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
-      -- indent-blankline character.
-      vim.g.indent_blankline_char = '▏'
+      require("indent_blankline").setup {
+        -- indent-blankline character.
+        indent_blankline_char = '▏',
 
-      -- Disable indent-blankline on these pages.
-      vim.g.indent_blankline_filetype_exclude = {'help', 'terminal', 'dashboard', 'packer', 'NvimTree'}
-      vim.g.indent_blankline_buftype_exclude = {'terminal'}
+        -- Disable indent-blankline on these pages.
+        indent_blankline_filetype_exclude = { 'help', 'terminal', 'dashboard', 'packer', 'NvimTree' },
+        indent_blankline_buftype_exclude = { 'terminal' },
 
-      vim.g.indent_blankline_show_first_indent_level = false
+        indent_blankline_show_first_indent_level = true,
+      }
     end
   }
 }
