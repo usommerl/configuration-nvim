7908fcad06fc9298243ffae7997e94819e5527d6
Uwe Sommerlatt <uwe.sommerlatt@exelonix.com>
2023-10-20 14:27:45 +0200

    Update several plugins and add markdown-preview

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -2,15 +2,19 @@ return {
   {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
-      require("indent_blankline").setup {
+      require("ibl").setup {
         -- indent-blankline character.
-        indent_blankline_char = '▏',
-
-        -- Disable indent-blankline on these pages.
-        indent_blankline_filetype_exclude = { 'help', 'terminal', 'dashboard', 'packer', 'NvimTree' },
-        indent_blankline_buftype_exclude = { 'terminal' },
-
-        indent_blankline_show_first_indent_level = true,
+        indent = { char = '▏' },
+        scope = {
+          show_start = false,
+          enabled = true,
+          exclude = { language = { 'scala' } }
+        },
+        exclude = {
+          filetypes = { 'help', 'terminal', 'dashboard', 'packer', 'NvimTree' },
+          buftypes = { 'terminal' },
+        }
+        -- indent_blankline_show_first_indent_level = true,
       }
     end
   }
