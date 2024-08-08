df90b798446e6d07f9d687078f211481b8d94581
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2024-08-08 09:10:07 +0200

    Make `checkhealth noice` happy :-)

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -5,12 +5,20 @@ return {
     dependencies = {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
+      "hrsh7th/nvim-cmp"
     },
     config = function()
       require("noice").setup({
-        presets = {
-          command_palette = true,
-          lsp_doc_border = true
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+          presets = {
+            command_palette = true,
+            lsp_doc_border = true
+          }
         },
         routes = {
           {
