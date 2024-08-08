707025788e3a66e21a3ed3f730b164bdc67e035e
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2024-08-08 08:46:41 +0200

    Update bufferline to version 4

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,7 +1,7 @@
 return {
   {
     'akinsho/bufferline.nvim',
-    version = "v3.*",
+    version = "v4.*",
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
       vim.opt.termguicolors = true
