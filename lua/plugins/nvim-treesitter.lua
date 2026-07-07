6c98f3ea045a22ad1c32f6fa048ebf7e9ebde83e
Uwe Sommerlatt <uwe.sommerlatt@gmail.com>
2026-07-07 15:43:40 +0200

    update: Use `main` branch of the `nvim-treesitter` repository

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,46 +1,37 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter.configs").setup(
-        {
-          ensure_installed = {
-            'angular',
-            'bash',
-            'css',
-            'csv',
-            'dart',
-            'java',
-            'json',
-            'jsonnet',
-            'lua',
-            'markdown',
-            'nix',
-            'python',
-            'ruby',
-            'rust',
-            'scala',
-            'scala',
-            'scss',
-            'toml',
-            'tsv',
-            'tsx',
-            'typescript',
-            'vim',
-            'vue',
-            'xml',
-            'yaml',
-            'zig',
-          },
-          highlight = {
-            enable = true,
-            disable = { 'gitcommit' }
-          },
-          incremental_selection = { enable = true },
-          indent = { enable = true }
-        })
+      require('nvim-treesitter').install({
+        'angular',
+        'bash',
+        'css',
+        'csv',
+        'dart',
+        'java',
+        'json',
+        'jsonnet',
+        'lua',
+        'markdown',
+        'nix',
+        'python',
+        'ruby',
+        'rust',
+        'scala',
+        'scala',
+        'scss',
+        'toml',
+        'tsv',
+        'tsx',
+        'typescript',
+        'vim',
+        'vue',
+        'xml',
+        'yaml',
+        'zig',
+      }):wait(300000)
     end
   }
 }
